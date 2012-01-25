7440fe6b7c5896b53daf8437adb7e6e008c73982
SteveW <SteveW@localhost>
2012-01-25 20:41:59 +0000

    Corrected NullReferenceException on setting DataEntryMode before strategy has been rendered.

--- Atdl4net/Properties/AssemblyInfo.cs
+++ Atdl4net/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.1077")]
-[assembly: AssemblyFileVersion("1.1.0.1077")]
+[assembly: AssemblyVersion("1.1.0.1078")]
+[assembly: AssemblyFileVersion("1.1.0.1078")]
 
