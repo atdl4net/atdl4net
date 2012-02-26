f71c46cdaed3add23686244da1efd5433214e617
SteveW <SteveW@localhost>
2012-02-26 12:39:23 +0000

    Added adjustment logic to convert from displayed local time to and from UTC wire values for UTCTimeOnly_t and UTCTimestamp_t.

--- Atdl4net/Model/Types/UTCDateOnly_t.cs
+++ Atdl4net/Model/Types/UTCDateOnly_t.cs
@@ -31,6 +31,8 @@ namespace Atdl4net.Model.Types
     /// special-purpose field is paired with UTCTimeOnly to form a proper UTCTimestamp for bandwidth-sensitive messages.
     /// Valid values: YYYY = 0000-9999, MM = 01-12, DD = 01-31.'
     /// </summary>
+    /// <remarks>Currently, UTCDateOnly_t does NOT inherit from UTCDateTimeBase as there is no reason to
+    /// apply any conversions to a date-only field.</remarks>
     public class UTCDateOnly_t : DateTimeTypeBase
     {
         private static readonly string[] _formatStrings = new string[] { FixDateTimeFormat.FixDateOnly };
